ff0e5cea80f3106876a4ae084389e60ffbdc61ab
ZhoskiyKostya <konstantinzateev2010@gmail.com>
2022-10-23 15:14:39 +0300

    продаю данные людей Бутану

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
-print('Hello world!')
-print('Goodbye world!')
-print('world!')
-print('ˇ')
+name = input('Введите ваше имя - ')
+bd = input('Введите ваш день рождения - ')
+os = input('Введите вашу ОС - ')
+bank = input('Введите данные вашей карточки (банковской) - ')
+user_data = f'Имя - {name}\nДР - {bd}\nOS - {os}\nДанные карточки - {bank}\n----\n'
+with open ('data.txt', 'a', encoding='utf-8') as f:
+    f.write(user_data)
+
 

e3544f7de256b3dd71d9116348b0ce2c19698406
ZhoskiyKostya <konstantinzateev2010@gmail.com>
2022-10-28 17:03:25 +0300

    добавил id и пароль

--- app.py
+++ app.py
@@ -1,9 +1,19 @@
+import random
+id1 = random.randint(0, 100000)
+passk = input('Введите ваш новый пароль - ')
 name = input('Введите ваше имя - ')
 bd = input('Введите ваш день рождения - ')
 os = input('Введите вашу ОС - ')
 bank = input('Введите данные вашей карточки (банковской) - ')
-user_data = f'Имя - {name}\nДР - {bd}\nOS - {os}\nДанные карточки - {bank}\n----\n'
+user_data = f'ID - {id1}\nИмя - {name}\nДР - {bd}\nOS - {os}\nДанные карточки - {bank}\n----\n'
 with open ('data.txt', 'a', encoding='utf-8') as f:
     f.write(user_data)
+passp = input('Введите ваш пароль - ')
+if passp != passk:
+    while True:
+        print(' ')
+else:
+    print('ну ок')
+
 
 
